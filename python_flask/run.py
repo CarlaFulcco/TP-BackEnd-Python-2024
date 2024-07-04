aee3f76155b4e02c0fba347b7af64923a3da84b4
CarlaFulcco <carlifulcco@gmail.com>
2024-07-04 00:03:45 -0300

    se adecua el front

--- python_flask/run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from python_flask.views import hoteleria
-
-python_flask = Flask(__name__)
-
-python_flask.route("/fourseasons", methods=["GET"])(hoteleria)
-
-if __name__=="__main__":
-    app.run(debug=True)
--- python_flask/views.py
+++
@@ -1,5 +0,0 @@
-from flask import jsonify
-
-def hoteleria():
-    response = { "title": "Four Seasons Hotel Madrid⭐⭐⭐⭐⭐"}
-    return jsonify(response)
